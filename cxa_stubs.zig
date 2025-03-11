d434097cd824dc2ec0451f5d7feb592ba0869578
Michael Belousov <michael.belousov98@gmail.com>
2025-03-11 10:03:51 -0400

    don't export cxa stubs on native platform

--- cxa_stubs.zig
+++ cxa_stubs.zig
@@ -1,6 +1,8 @@
+const builtin = @import("builtin");
+
 // https://refspecs.linuxfoundation.org/abi-eh-1.22.html#cxx-throw
 //
-export fn __cxa_allocate_exception(_thrown_size: usize) ?*anyopaque {
+fn __cxa_allocate_exception(_thrown_size: usize) callconv(.c) ?*anyopaque {
     _ = _thrown_size;
     @panic("__cxa_allocate_exception");
 }
@@ -10,9 +12,16 @@ const StdCppTypeInfo = opaque {};
 // should be: fn (?*anyopaque) void
 const Destructor = opaque {};
 
-export fn __cxa_throw(_thrown: ?*anyopaque, _type_info: ?*StdCppTypeInfo, _destructor: ?*Destructor) void {
+fn __cxa_throw(_thrown: ?*anyopaque, _type_info: ?*StdCppTypeInfo, _destructor: ?*Destructor) callconv(.c) void {
     _ = _thrown;
     _ = _type_info;
     _ = _destructor;
     @panic("__cxa_throw");
+}
+
+comptime {
+    if (builtin.cpu.arch.isWasm()) {
+        @export(&__cxa_throw, .{ .name = "__cxa_throw" });
+        @export(&__cxa_allocate_exception, .{ .name = "__cxa_allocate_exception" });
+    }
 }
